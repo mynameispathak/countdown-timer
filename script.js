411c5ccbd4168f27fa64ae18ff94ce0af6988099
Aniket Pathak <171230012@gmail.com>
2018-08-19 00:53:27 +0530

    Updated Script.js

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-const buttons = document.querySelectorAll('.countdown-clock-controls button'), enterTime = document.querySelector('.countdown-clock-controls input'),
+const buttons = document.querySelectorAll('.countdown-clock-controls button'),
+      enterTime = document.querySelector('.countdown-clock-controls input'),
       clock = document.querySelector('.clock'),
       backAt = document.querySelector('.back-at');
 
 let timer;
 
-function startTimer(seconds){
-	clearInterval(timer);
+function startTimer(seconds) {
+  clearInterval(timer);
 
-	let now = Date.now(),
-		then = now + (seconds * 1000);
+  let now = Date.now(),
+      then = now + (seconds * 1000);
 
-	displayTimeLeft(seconds);
-	displayBackaAt(then);
+  displayTimeLeft(seconds);
+  displayBackAt(then);
 
-	timer = setInterval(() => {
-		let secsLeft = Math.round((then - Date.now()) / 1000);
+  timer = setInterval(() => {
+    let secsLeft = Math.round((then - Date.now()) / 1000);
 
-		if (secsLeft < 0) {
-			clearInterval(timer);
-			return;
-		}
+    if (secsLeft < 0) {
+      clearInterval(timer);
+      return;
+    }
 
-		displayTimeLeft(secsLeft);
-	}, 1000);
+    displayTimeLeft(secsLeft);
+  }, 1000);
 }
 
-function displayTimeLeft(secsLeft){
-	let minutes = Math.floor(secsLeft / 60),
-		secs = secsLeft % 60;
+function displayTimeLeft(secsLeft) {
+  let minutes = Math.floor(secsLeft / 60),
+      secs = secsLeft % 60;
 
-	clock.textContent = '${formatTime(minutes)}:${formatTime(secs)}';
+  clock.textContent = `${formatTime(minutes)}:${formatTime(secs)}`;
 }
 
-function displayBackAt(then){
-	let end = new Date(then),
-		hours = end.gethours(),
-		minutes = end.getMinutes();
+function displayBackAt(then) {
+  let end = new Date(then),
+      hours = end.getHours(),
+      minutes = end.getMinutes();
 
-	backAt.textContent = 'Back at ${formatTime(hours)}:${formatTime(minutes)}';
+  backAt.textContent = `Back at ${formatTime(hours)}:${formatTime(minutes)}`;
 }
 
-function formatTime(time){
-	return '${time < 10 ? '0' : ''}${time}';
+function formatTime(time) {
+  return `${time < 10 ? '0' : ''}${time}`;
 }
 
-function setSeconds(e){
-	let value = parseInt(this.value),
-		secs = e.type === 'click' ? value : value * 60;
+function setSeconds(e) {
+  let value = parseInt(this.value),
+      secs = e.type === 'click' ? value : value * 60;
 
-	startTimer(secs);
+  startTimer(secs);
 }
 
 buttons.forEach(button => button.addEventListener('click', setSeconds));
